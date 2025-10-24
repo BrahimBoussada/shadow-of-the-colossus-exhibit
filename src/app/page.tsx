68c7a9ca0f36784f081c27215babd82d8800f1d8
Brahim Boussada <brahimboussada@protonmail.com>
2025-10-25 01:41:40 +0300

    feat: add game cover to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <div>Hi</div>;
+  return (
+    <div className=" flex-col flex items-center justify-center">
+      <div className="relative max-w-xs mx-auto mb-4">
+        <Image
+          className="w-full"
+          src={"/game_cover.jpg"}
+          height={1051}
+          width={1579}
+          alt={"Shadow of the Colossus Game Cover"}
+          priority
+          quality={100}
+        />
+      </div>
+    </div>
+  );
 }
