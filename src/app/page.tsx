bf2edba8ed184ba3e3da55672f3e384db4a99c8f
Brahim Boussada <brahimboussada@protonmail.com>
2025-10-25 01:48:50 +0300

    feat: add game description to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className=" flex-col flex items-center justify-center">
       <div className="flex flex-col gap-4">
         {/* GAME TITLE LOGO */}
-        <div className="relative max-w-xs mx-auto mb-4">
+        <h1 className="relative max-w-xs mx-auto mb-4">
           <Image
             className="w-full"
             src={"/game_logo.png"}
@@ -15,7 +15,7 @@ export default function Home() {
             priority
             quality={100}
           />
-        </div>
+        </h1>
 
         {/* GAME COVER */}
         <div className="relative max-w-xs mx-auto">
@@ -29,6 +29,18 @@ export default function Home() {
             quality={100}
           />
         </div>
+
+        <h2 className="md:text-center max-w-2xl text-neutral-800 text-balance md:text-sm">
+          Shadow of the Colossus is an action-adventure game developed by Team
+          Ico and released in 2005 for the PlayStation 2, later remastered for
+          subsequent platforms. Set in a vast, desolate world, players control
+          Wander, a young man on a quest to revive a girl named Mono by
+          defeating sixteen massive creatures known as Colossi. The game is
+          renowned for its minimalist storytelling, breathtaking visuals, and
+          innovative gameplay, blending exploration, puzzle-solving, and epic
+          boss battles. Its emotional depth and atmospheric design have made it
+          a critically acclaimed classic.
+        </h2>
       </div>
     </div>
   );
