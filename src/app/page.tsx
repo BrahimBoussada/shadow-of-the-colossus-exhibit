a3c41b8b7074b4c8515f7b9a81a57bfb324cf385
Brahim Boussada <brahimboussada@protonmail.com>
2025-10-25 01:46:16 +0300

    feat: add game title logo to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,32 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className=" flex-col flex items-center justify-center">
-      <div className="relative max-w-xs mx-auto mb-4">
-        <Image
-          className="w-full"
-          src={"/game_cover.jpg"}
-          height={1051}
-          width={1579}
-          alt={"Shadow of the Colossus Game Cover"}
-          priority
-          quality={100}
-        />
+      <div className="flex flex-col gap-4">
+        {/* GAME TITLE LOGO */}
+        <div className="relative max-w-xs mx-auto mb-4">
+          <Image
+            className="w-full"
+            src={"/game_logo.png"}
+            height={1921}
+            width={694}
+            alt={"Shadow of the Colossus Game Title Logo"}
+            priority
+            quality={100}
+          />
+        </div>
+
+        {/* GAME COVER */}
+        <div className="relative max-w-xs mx-auto">
+          <Image
+            className="w-full"
+            src={"/game_cover.jpg"}
+            height={1051}
+            width={1579}
+            alt={"Shadow of the Colossus Game Cover"}
+            priority
+            quality={100}
+          />
+        </div>
       </div>
     </div>
   );
